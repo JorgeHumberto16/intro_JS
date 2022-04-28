480aa899f0dbc44bfabcfb36fa3c087f0bcc501e
JorgeHumberto16 <83613918+JorgeHumberto16@users.noreply.github.com>
2022-04-27 23:56:49 -0500

    Add files via upload

---
+++ Actividad3/Problema3.js
@@ -0,0 +1,3 @@
+var num = Number(prompt("Hola! Ingresa tu número: "))
+if (num == 1000) alert("FELICIDADES!! Ganaste un premio")
+else alert("Lo sentimos, sigue participando")
---
+++ Actividad3/Problema4.js
@@ -0,0 +1,4 @@
+var num1 = Number(prompt("Ingresa tu primer numero: "))
+var num2 = Number(prompt("Ingresa tu segundo numero: "))
+if (num1 > num2) console.log(num1, "es mayor que", num2)
+else if (num1 < num2) console.log(num2, "es mayor que", num1)
