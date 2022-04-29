3b3406d1f53b9fae7bd9e7be9f681b39e88f1cf5
JorgeHumberto16 <83613918+JorgeHumberto16@users.noreply.github.com>
2022-04-28 21:40:05 -0500

    Add files via upload

---
+++ Actividad4/Actividad2.js
@@ -0,0 +1,6 @@
+var num = Number(prompt("Ingresa el numero: "))
+var i=0
+while(i<=num){
+    i++
+    if(i % 5 == 0) console.log(i," es multiplo de 5")
+}
---
+++ Actividad4/Actividad3.js
@@ -0,0 +1,6 @@
+var num = Number(prompt("Ingresa el numero: "))
+var i=0
+do{
+    i++
+    if(i % 5 == 0) console.log(i," es multiplo de 5")
+}while(i<=num)
